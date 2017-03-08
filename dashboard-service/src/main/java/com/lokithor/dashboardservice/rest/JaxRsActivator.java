6cad16b716c9c60b0364e7e39477132fdf6531e6
Leonardo Cruz <leolsc@yahoo.com.br>
2017-03-08 14:36:29 -0800

    Activating Jax-rs and CDI.

---
+++ dashboard-service/src/main/java/com/lokithor/dashboardservice/rest/JaxRsActivator.java
@@ -0,0 +1,8 @@
+package com.lokithor.dashboardservice.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JaxRsActivator extends Application {
+}
