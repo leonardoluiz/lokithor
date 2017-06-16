46caeea5e8ded3cbe809f4c706d571c5be6f66ed
Leonardo Cruz <leolsc@yahoo.com.br>
2017-06-16 13:27:37 -0700

    Renaming resource's names

--- geotracking-service/src/main/java/com/lokithor/geotracking/rest/DeviceResource.java
+++ geotracking-service/src/main/java/com/lokithor/geotracking/rest/DeviceResource.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/device")
+@Path("/devices")
 public class DeviceResource {
 
     @Inject
--- geotracking-service/src/main/java/com/lokithor/geotracking/rest/VersionResource.java
+++ geotracking-service/src/main/java/com/lokithor/geotracking/rest/VersionResource.java
@@ -7,17 +7,17 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/about")
-public class AboutResource {
+@Path("/version")
+public class VersionResource {
 
-    private class About {
-        public String version = "v 0.1 beta";
+    private class Version {
+        public String version = "v 0.0.1";
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response about() {
-        return Response.ok(new About()).build();
+        return Response.ok(new Version()).build();
     }
 
 }
